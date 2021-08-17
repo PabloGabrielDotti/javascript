5a29de600078fd09cec3f860d42d99bd9a73b5e2
Pablo <paablo.92@hotmail.es>
2021-08-17 18:33:40 -0300

    practico en holamundo y creo ejercicios

---
+++ js/holaMundo.js
@@ -0,0 +1,41 @@
+// esto es un comentario simple
+/* esto es un comentario
+de multimples
+lineas */
+
+
+//mostrar un mensaje por consola
+console.log("hola comision")
+
+//mostrar mensaje en pagina
+document.write("este es mi primer mensaje")
+
+//mostrar mensaje en pagina interpretando html
+document.write("<h1>este es mi primer mensaje</h1>")
+
+//mostrar una ventana emergente 
+//alert("esto es una alerta")
+
+//crear o declarar una variable var - let - const
+
+let palabraBuscada = "hola";
+
+document.write("<br>Variable palabra buscada:" + palabraBuscada);
+
+//modificar variable
+
+palabraBuscada = "chau";
+
+document.write("<br>Variable palabra buscada:" + palabraBuscada);
+
+//establecer constante
+
+const comision = 17;
+
+document.write("<br>Variable comision:" + comision);
+
+let luz = true;
+
+let suma = 20;
+
+
