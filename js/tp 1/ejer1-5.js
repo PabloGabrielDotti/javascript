9c1996af600bdd546fa01178617d8782fdf50570
Pablo <paablo.92@hotmail.es>
2021-09-15 20:09:50 -0300

    muevo arreglos y bucles afuera de carpeta

--- js/tp 1/ejer1-5.js
+++ js/tp 1/ejer1-5.js
@@ -1,6 +1,6 @@
 // parseInt ("numero") se transforma en formato entero
 
-
+/*
 
 let numero1 = parseInt(prompt("ingrese un numero"));
 let numero2 = 50;
@@ -12,5 +12,21 @@ console.log(numero1+numero2);
 document.write("el resultado de la suma es <br>" + (numero1 + numero2));
 
 
+*/
+/*
 
+a="pp"
+alert(a)
+*/
 
+let numero1 = parseInt(prompt("ingrese un numero"));
+let numero2 = parseInt(prompt("ingrese un numero"));
+let numero3 = parseInt(prompt("ingrese un numero"));
+
+if (numero1 > numero2 && numero1 > numero3) {
+  document.write(numero1);
+} else if (numero2 > numero1 && numero2 > numero3) {
+    document.write(numero2);
+} else {
+  document.write(numero3);
+}
--- js/tp 2/ejercicio7.js
+++ js/tp 2/ejercicio7.js
@@ -1,11 +1,8 @@
+let j = parseInt(prompt("ingrese un numero del 1 al 50"));
 
-
-
-
-for(let i=parseInt(prompt("ingrese un numero del 1 al 50")); i<=50;i--){
-    for (let x=i; x<=i;x--){
-        document.write(i)
-    
-    }
-document.write("<br>")
+for (let i = j; i <= 1; i--) {
+  for (let x = i; x <= i; x--) {
+    document.write(i);
+  }
+  document.write("<br>");
 }
